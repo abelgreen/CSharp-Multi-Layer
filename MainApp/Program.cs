11b9abc6175e4ee46ef85b6651695c05265c3d22
abel green <abel.green@outlook.com>
2017-08-01 10:36:19 -0400

    first_initial

---
+++ MainApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GUI;
+
+namespace MainApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// Sielence is Gold
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new HiTech());
+        }
+    }
+}
